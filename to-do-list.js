e27f11ba562601ec7cc0f85e14caab38bed5e768
Diamond Designer <diamondd3signer@gmail.com>
2023-08-09 15:48:48 -0500

    tasks can now be added

--- to-do-list.js
+++ to-do-list.js
@@ -1,10 +1,21 @@
+// tests to see if user input is being received
 
-document.getElementById('add-task').onclick = function () {
+document.getElementById ('add-task').onclick = function () {
    let task = document.getElementById('task').value
-   console.log(task);
+   console.log(task); // check the console to see if the task in the text box is being displayed
+
+   // refers to the container of the tasksk
+   let parentElement = document.getElementById('tasks-container');
+
+// creates a task
+   let newTask = document.createElement('div');
+   newTask.textContent = task;
+   newTask.className = "task";
+
+// appends the task into the container
+   parentElement.appendChild(newTask);
+
 }
 
-function receiveTask() {
-   /* receive user imput and renders an element  onto the page */
 
-}
+
