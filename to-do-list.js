d5ddf02f2f0c15e2dda63392b58fec7b38e2e7f3
Diamond Designer <diamondd3signer@gmail.com>
2023-08-09 14:34:43 -0500

    initial test phase

--- to-do-list.js
+++ to-do-list.js
@@ -1,6 +1,10 @@
 
+document.getElementById('add-task').onclick = function () {
+   let task = document.getElementById('task').value
+   console.log(task);
+}
 
-function receiveTask () {
+function receiveTask() {
    /* receive user imput and renders an element  onto the page */
 
 }
